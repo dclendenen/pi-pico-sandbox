28a0f6f0a2b2beb2597f305f48a6c98f3a17580e
David Clendenen <dclendenen@gmail.com>
2021-09-18 18:32:09 -0600

    got it working, baseline

--- main.py
+++ main.py
@@ -2,25 +2,24 @@ from machine import Pin
 import time
 
 led_pin = Pin(25, Pin.OUT)
-trigger_pin = Pin(17, Pin.OUT)
-echo_pin = Pin(19, Pin.IN)
+trigger_pin = Pin(13, Pin.OUT)
+echo_pin = Pin(15, Pin.IN)
 
 def pulse_gen(pulse_pin, pulse_on_time):
-    """Generate a single positive pulse of duration ``pulse_on_time`` on output pin ``pulse_pin`` """
+    """Generate a single positive pulse of duration ``pulse_on_time`` (us) on output pin ``pulse_pin`` """
     pulse_pin.high()
-    time.sleep_ms(pulse_on_time)
+    time.sleep_us(pulse_on_time)
     pulse_pin.low()
 
 def echo_pin_irq():
-    print("Echo")
+    led_pin.toggle()
 
 
-echo_pin.irq(echo_pin_irq, Pin.IRQ_RISING, hard=True)
+echo_pin.irq(echo_pin_irq, Pin.IRQ_RISING, hard=False)
 
 
 
 
 while True:
     pulse_gen(trigger_pin, 20)
-    pulse_gen(led_pin, 1000)
     time.sleep_ms(1000)
